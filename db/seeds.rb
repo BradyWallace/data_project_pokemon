ed1f2d97a770522ef080ce055a691f0b1f3b820a
Brady Wallace <bwallace2@academic.rrc.ca>
2023-10-04 15:58:15 -0500

    seeding types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+require "net/http"
+require "json"
+
+Type.delete_all
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='type';")
+
+# Seeding Types from CSV file
+filename = Rails.root.join("db/types.csv")
+puts "Loading Types from the CSV file: #{filename}"
+csv_data = File.read(filename)
+types = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+types.each do |t|
+  type = Type.create(
+    name: t["name"]
+  )
+  puts "Invalid type #{t['name']}" unless type&.valid?
+end
+
+puts "Created #{Type.count} Types."
