de0e14465c288e95e5ca8f97febeda5d530644c5
Brady Wallace <bwallace2@academic.rrc.ca>
2023-10-23 23:33:57 -0500

    added types pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
   root to: "home#index"
+  resources :pokemons, only: %i[index show]
+  resources :types, only: %i[index show]
+  resources :abilities, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
